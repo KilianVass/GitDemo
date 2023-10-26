1a7acd00330e5d1811ae84fc17f9bc6305599044
Kilian <kilianvass@gmail.com>
2023-10-26 20:55:47 +0200

    Nuevo

--- Appium/src/test/java/Kilian/Appium/JavaIntroduction.java
+++ Appium/src/test/java/Kilian/Appium/JavaIntroduction.java
@@ -4,7 +4,7 @@ public class JavaIntroduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola git cambios");
+		System.out.print("Hola git cambios y más cambios");
 		int number = 5;
 		String frase = "KilianJava";
 		char letra = 'k';
